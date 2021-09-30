2ce2f963622ffc746c57a596fdedad24cdbfc860
jstaman5 <jstaman5@gmail.com>
2021-09-30 16:37:18 -0400

    starting project

--- Project4/diffie-hellman.py
+++ Project4/diffie-hellman.py
@@ -1,8 +1,19 @@
 #Jared Staman
 #CS 583 Project 4: Diffie-Hellman
 
-def main():
-    print("hello")
+from Crypto.Util.number import getRandomInteger
+from Crypto.Util.number import getPrime
+from Crypto.Util.number import isPrime
+
+def getStrongPrime(bits):
+    check = 0
+    while(check == 0):
+        num = getPrime(bits)
+        num2 = int( (num-1) / 2)
+        if( isPrime( num2)):
+            check = 1
+
+    print(num)
     return
 
-main()
+getStrongPrime(1024)
